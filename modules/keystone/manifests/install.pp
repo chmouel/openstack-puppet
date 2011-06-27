1a9250e7927f914ee3204ab87b67176e42523d67
Chmouel Boudjnah <chmouel@chmouel.com>
2011-06-27 18:47:23 -0500

    Ensure keystone log dir/files are keystone chowned

--- modules/keystone/manifests/install.pp
+++ modules/keystone/manifests/install.pp
@@ -3,7 +3,20 @@ class keystone::install {
   package { "keystone":
     ensure => present
   }
-  
+
+  file { "/var/log/keystone":
+    ensure => directory,
+    owner  => "keystone",
+    mode   => 0755
+  }
+
+  file { "/var/log/keystone/keystone.log":
+    ensure => present,
+    owner  => "keystone",
+    mode   => "600",
+    require => File["/var/log/keystone"]
+  }
+
   file { "initial_data.sh":
     path => "/var/lib/keystone/initial_data.sh",
     ensure  => present,
@@ -20,7 +33,9 @@ class keystone::install {
     unless      => "keystone-manage user list | grep -q admin",
     require     => [
       Package['keystone'],
+      File['/var/log/keystone/keystone.log'],
       File["initial_data.sh"]
+      
     ]
   }
 
